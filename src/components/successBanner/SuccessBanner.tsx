7bcdf4eaaab9354587efd1a7955fcbec41af9192
Yuriy Khoptyanii <hoptyanoyyura@gmail.com>
2023-07-07 12:39:38 +0300

    fixed bug with success image

--- src/components/successBanner/SuccessBanner.tsx
+++ src/components/successBanner/SuccessBanner.tsx
@@ -5,7 +5,9 @@ import successImg from '../../assets/success-image-new.svg';
 const SuccessBanner = () => (
   <div className="successBanner">
     <h1 className="successBanner__title">User successfully registered</h1>
-    <img src={successImg} alt="succeed registration" className="successBanner__img" />
+    <div className="containerImg">
+      <img src={successImg} alt="succeed registration" className="successBanner__img" />
+    </div>
   </div>
 );
 
